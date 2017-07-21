a60c7234cb248d90a904a685dfe53fe173812712
hi-PC\Administrator <dhgustn3721@naver.com>
2017-07-21 18:30:32 +0900

    no message

---
+++ js/gnb.js
@@ -0,0 +1,20 @@
+/**
+ * gnb jQuery
+ */
+
+$(function(){
+
+  $('.gnb-depth1-link').on('mouseenter', function(){
+
+    $(this).next('.gnb-depth2').addClass('on');
+
+  });
+
+  //$('.gnb-depth1-item').on('mouseleave', function(){
+  //
+  //  $(this).children('.gnb-depth2').css('display', 'none');
+  //
+  //});
+
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIl9nbmIuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiZ25iLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXHJcbiAqIGduYiBqUXVlcnlcclxuICovXHJcblxyXG4kKGZ1bmN0aW9uKCl7XHJcblxyXG4gICQoJy5nbmItZGVwdGgxLWxpbmsnKS5vbignbW91c2VlbnRlcicsIGZ1bmN0aW9uKCl7XHJcblxyXG4gICAgJCh0aGlzKS5uZXh0KCcuZ25iLWRlcHRoMicpLmFkZENsYXNzKCdvbicpO1xyXG5cclxuICB9KTtcclxuXHJcbiAgLy8kKCcuZ25iLWRlcHRoMS1pdGVtJykub24oJ21vdXNlbGVhdmUnLCBmdW5jdGlvbigpe1xyXG4gIC8vXHJcbiAgLy8gICQodGhpcykuY2hpbGRyZW4oJy5nbmItZGVwdGgyJykuY3NzKCdkaXNwbGF5JywgJ25vbmUnKTtcclxuICAvL1xyXG4gIC8vfSk7XHJcblxyXG59KTsiXX0=
